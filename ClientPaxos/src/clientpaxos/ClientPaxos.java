55f2fd7c325122dac8a198ec07e56713a30825d1
Yoga Adrian <yoga.adrians14@gmail.com>
2016-04-25 17:13:59 +0700

    tcp dari jarkom

--- ClientPaxos/src/clientpaxos/ClientPaxos.java
+++ ClientPaxos/src/clientpaxos/ClientPaxos.java
@@ -5,17 +5,73 @@
  */
 package clientpaxos;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author yoga
  */
 public class ClientPaxos {
 
+    static public Socket clientSocket;
+    static public BufferedReader objectFromServer;
+    static public PrintWriter objectToServer;
+    static public Scanner scan;
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Input server IP hostname : ");
+        String host = scan.nextLine();
+        clientSocket = new Socket(host, 2000);
+        Thread t = new Thread(new StringGetter());
+        t.start();
+        while (true) {
+
+        }
+    }
+    
+    public static class StringGetter
+            extends Thread {
+
+        public void run() {
+            try {
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String response;
+                while (true) {
+                    response = inFromServer.readLine();
+                    if (response.length()>12) {
+                        if (!response.substring(0,12).equals("update-board"))
+                            System.out.println(response);
+                    }
+     
+
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(ClientPaxos.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public static void sendToServer(String msg) throws Exception {
+        //create output stream attached to socket
+        PrintWriter outToServer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        //send msg to server
+        outToServer.print(msg + '\n');
+        outToServer.flush();
     }
     
 }
--- ServerPaxos/src/serverpaxos/ServerPaxos.java
+++ ServerPaxos/src/serverpaxos/ServerPaxos.java
@@ -5,17 +5,85 @@
  */
 package serverpaxos;
 
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author yoga
  */
 public class ServerPaxos {
+    static public ServerSocket server;
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        InetAddress ip;
+        String hostname;
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+            System.out.println("Server IP address : " + ip);
+            System.out.println("Server IP hostname : " + hostname);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        server = new ServerSocket(2000);
+        while (true) {
+            Socket socket = server.accept();
+            System.out.println("connected");
+            ClientController clientcontroller = new ClientController(socket);
+
+            Thread t = new Thread(clientcontroller);
+            t.start();
+        }
+     
         // TODO code application logic here
     }
+    
+    
+    public static class ClientController
+            extends Thread {
+
+        public Socket socket;
+
+        public ClientController(Socket clientSocket) {
+            this.socket = clientSocket;
+        }
+
+        void SendToClient(String msg) throws Exception {
+            //create output stream attached to socket
+            PrintWriter outToClient = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+            //send msg to server
+            outToClient.print(msg + '\n');
+            outToClient.flush();
+        }
+
+        public void run() {
+            try {
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String request;
+            } catch (SocketException ex) {
+            } catch (IOException ex) {
+                Logger.getLogger(ServerPaxos.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (Exception ex) {
+                Logger.getLogger(ServerPaxos.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    
     
 }
