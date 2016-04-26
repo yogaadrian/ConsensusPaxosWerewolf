f7f150a565c81f3b2ffb9cb61c1f7a2b531dcb7d
Yoga Adrian <yoga.adrians14@gmail.com>
2016-04-26 13:10:51 +0700

    first function

--- ClientPaxos/src/clientpaxos/ClientPaxos.java
+++ ClientPaxos/src/clientpaxos/ClientPaxos.java
@@ -19,6 +19,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -29,7 +32,7 @@ public class ClientPaxos {
     static public Socket clientSocket;
     static public BufferedReader objectFromServer;
     static public PrintWriter objectToServer;
-    static public Scanner scan;
+    static public Scanner scan=new Scanner(System.in);
 
     /**
      * @param args the command line arguments
@@ -38,36 +41,40 @@ public class ClientPaxos {
         // TODO code application logic here
         //cobain udp
         /*
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Enter menu: ");
-        int menu = reader.nextInt();
-        reader.nextLine();
-        String menuString;
-        switch(menu) {
-            case 1: menuString = "Send";
-            break;
-            case 2: menuString = "Receive";
-            break;
-            default : menuString = "Invalid";
-        }
-        System.out.println(menuString);
-        if (menu == 1) {
-            System.out.println("Enter IP: ");
-            String IP = reader.nextLine();
-            System.out.println("Enter target port: ");
-            int targetPort = reader.nextInt();
-            sendMessage(IP, targetPort);
-        } else if(menu == 2) {
-            System.out.println("Enter listen port: ");
-            int listenPort = reader.nextInt();
-            receiveMessage(listenPort);
-        }*/
-        
+         Scanner reader = new Scanner(System.in);
+         System.out.println("Enter menu: ");
+         int menu = reader.nextInt();
+         reader.nextLine();
+         String menuString;
+         switch(menu) {
+         case 1: menuString = "Send";
+         break;
+         case 2: menuString = "Receive";
+         break;
+         default : menuString = "Invalid";
+         }
+         System.out.println(menuString);
+         if (menu == 1) {
+         System.out.println("Enter IP: ");
+         String IP = reader.nextLine();
+         System.out.println("Enter target port: ");
+         int targetPort = reader.nextInt();
+         sendMessage(IP, targetPort);
+         } else if(menu == 2) {
+         System.out.println("Enter listen port: ");
+         int listenPort = reader.nextInt();
+         receiveMessage(listenPort);
+         }*/
+
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Input server IP hostname : ");
         String host = scan.nextLine();
-        clientSocket = new Socket(host, 2000);
+        System.out.print("Input server Port : ");
+        int port = scan.nextInt();
+        scan.nextLine();
+        clientSocket = new Socket(host, port);
+        System.out.println("Connected");
         Thread t = new Thread(new StringGetter());
         t.start();
         while (true) {
@@ -75,10 +82,31 @@ public class ClientPaxos {
             System.out.print("COMMAND : ");
             //send msg to server
             String msg = scan.nextLine();
-            sendToServer(msg);
+            Parse(msg);
         }
     }
-    
+
+    public static void Parse(String msg) throws Exception {
+        if (msg.equals("join")) {
+            String json ;
+            //build jsonObject
+            JSONObject jsonObject = new JSONObject();
+            System.out.print("Masukkan nama : ");
+            String username = scan.nextLine();
+            jsonObject.put("method", "join");
+            jsonObject.put("username", username);
+            jsonObject.put("udp_address", InetAddress.getLocalHost().getHostAddress());
+            System.out.print("Masukkan port : ");
+            int port=scan.nextInt();
+            scan.nextLine();
+            jsonObject.put("udp_port", port);
+            //convert JSONObject to JSON to String
+            json = jsonObject.toString();
+            System.out.println(json);
+            sendToServer(json);
+        }
+    }
+
     public static class StringGetter
             extends Thread {
 
@@ -89,13 +117,33 @@ public class ClientPaxos {
                 while (true) {
                     response = inFromServer.readLine();
                     System.out.println(response);
+                    Parse(response);
                 }
             } catch (IOException ex) {
                 Logger.getLogger(ClientPaxos.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ParseException ex) {
+                Logger.getLogger(ClientPaxos.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        
+        public void Parse(String str) throws ParseException{
+            JSONParser parser = new JSONParser();
+            JSONObject json = (JSONObject) parser.parse(str);
+            String status = (String) json.get("status");
+            if (status.equals("fail")) {
+                String description = (String) json.get("description");
+                System.out.println(description);
+            } else if (status.equals("error")){
+                String description = (String) json.get("description");
+                System.out.println(description);
+            } else if(status.equals("ok")){
+                //isi disini
+                System.out.println(str);
             }
         }
     }
-    
+
     public static void sendToServer(String msg) throws Exception {
         //create output stream attached to socket
         PrintWriter outToServer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
@@ -103,15 +151,15 @@ public class ClientPaxos {
         outToServer.print(msg + '\n');
         outToServer.flush();
     }
-    
+
     public static void sendMessage(String IP, int targetPort) throws Exception {
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 
         InetAddress IPAddress = InetAddress.getByName(IP);
 
-	DatagramSocket datagramSocket = new DatagramSocket();
-	UnreliableSender unreliableSender = new UnreliableSender(datagramSocket);
-	
+        DatagramSocket datagramSocket = new DatagramSocket();
+        UnreliableSender unreliableSender = new UnreliableSender(datagramSocket);
+
         while (true) {
             String sentence = inFromUser.readLine();
             if (sentence.equals("quit")) {
@@ -124,12 +172,12 @@ public class ClientPaxos {
         }
         datagramSocket.close();
     }
-    
-    public static void receiveMessage(int listenPort) throws Exception {
-	DatagramSocket serverSocket = new DatagramSocket(listenPort);
 
-	byte[] receiveData = new byte[1024];
-	while(true) {
+    public static void receiveMessage(int listenPort) throws Exception {
+        DatagramSocket serverSocket = new DatagramSocket(listenPort);
+
+        byte[] receiveData = new byte[1024];
+        while (true) {
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
 
@@ -137,5 +185,5 @@ public class ClientPaxos {
             System.out.println("RECEIVED: " + sentence);
         }
     }
-    
+
 }
--- ServerPaxos/src/serverpaxos/ServerPaxos.java
+++ ServerPaxos/src/serverpaxos/ServerPaxos.java
@@ -16,44 +16,52 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import player.Player;
 
 /**
  *
  * @author yoga
  */
 public class ServerPaxos {
+
     static public ServerSocket server;
+    static public int totalPlayer = 0;
+    static public int totalReady = 0;
+    static public ArrayList<Player> listPlayer = new ArrayList();
+    static public boolean play = false;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        InetAddress ip;
+        String ip;
         String hostname;
         try {
-            ip = InetAddress.getLocalHost();
-            hostname = ip.getHostName();
+            ip = InetAddress.getLocalHost().getHostAddress();
             System.out.println("Server IP address : " + ip);
-            System.out.println("Server IP hostname : " + hostname);
+            System.out.println("Port : 2000");
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
         server = new ServerSocket(2000);
         while (true) {
             Socket socket = server.accept();
-            System.out.println("connected");
+            System.out.println("Connected");
             ClientController clientcontroller = new ClientController(socket);
 
             Thread t = new Thread(clientcontroller);
             t.start();
         }
-     
+
         // TODO code application logic here
     }
-    
-    
+
     public static class ClientController
             extends Thread {
 
@@ -71,14 +79,72 @@ public class ServerPaxos {
             outToClient.flush();
         }
 
+        void Parse(String msg) throws ParseException, Exception {
+            JSONParser parser = new JSONParser();
+            JSONObject json = (JSONObject) parser.parse(msg);
+            String method = (String) json.get("method");
+            System.out.println(method);
+            if (method.equals("join")) {
+                String username = (String) json.get("username");
+                if (checkuser(username) && !play) {
+                    listPlayer.add(new Player(totalPlayer++, 1, (String) json.get("udp_address"), Integer.parseInt(json.get("udp_port").toString()), username));
+                    String response;
+                    //build jsonObject
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject.put("status", "ok");
+                    jsonObject.put("player_id", totalPlayer - 1);
+
+                    //convert JSONObject to JSON to String
+                    response = jsonObject.toString();
+                    System.out.println("kirim : " +response);
+                    SendToClient(response);
+                } else if (!checkuser(username)) {
+                    String response;
+                    //build jsonObject
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject.put("status", "fail");
+                    jsonObject.put("description", "user exists");
+
+                    //convert JSONObject to JSON to String
+                    response = jsonObject.toString();
+                    System.out.println(json);
+                    SendToClient(response);
+
+                } else if(play){
+                     String response;
+                    //build jsonObject
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject.put("status", "fail");
+                    jsonObject.put("description", "please wait, game is currently running");
+
+                    //convert JSONObject to JSON to String
+                    response = jsonObject.toString();
+                    System.out.println(json);
+                    SendToClient(response);
+                } 
+            }
+        }
+
+        boolean checkuser(String username) {
+            for (int i = 0; i < listPlayer.size(); i++) {
+                if (listPlayer.get(i).getUsername().equals(username)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
         public void run() {
             try {
+                System.out.println(socket.getInetAddress().toString() + ':' + socket.getPort());
                 BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 String request;
                 while ((request = inFromClient.readLine()) != null) {
                     System.out.println(request);
+                    Parse(request);
                 }
             } catch (SocketException ex) {
+                System.out.println("disconnect 1 client");
             } catch (IOException ex) {
                 Logger.getLogger(ServerPaxos.class.getName()).log(Level.SEVERE, null, ex);
             } catch (Exception ex) {
@@ -86,7 +152,5 @@ public class ServerPaxos {
             }
         }
     }
-    
-    
-    
+
 }
